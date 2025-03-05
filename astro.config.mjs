81fcd644ef7cab3a06000bc232cef0ed4fbad82c
Simen A. W. Olsen <hello@simenandre.no>
2025-03-05 13:54:03 +0100

    many improvements; seo, add sharing image, etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://sokkel.io",
+
   experimental: {
     responsiveImages: true,
   },
+
+  integrations: [sitemap()],
 });
