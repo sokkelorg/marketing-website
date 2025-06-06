aaf361c4cabe5add150f02a692a7eac163ae7020
Simen A. W. Olsen <hello@simenandre.no>
2025-06-06 14:25:32 +0200

    chore: redirect to deck

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,4 +13,8 @@ export default defineConfig({
   },
 
   integrations: [sitemap(), lottie()],
+
+  redirects: {
+    '/2025-06-deck': 'https://www.figma.com/deck/LePYMT44A7Tfq4k0wGwG1K/Sokkel---Pitch-Deck--Presentation-?node-id=14-649&viewport=-130%2C-138%2C0.61&t=ELDthSvGVH51kDVR-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1',
+  },
 });
