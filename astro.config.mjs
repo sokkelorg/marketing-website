bef24ef9bdc2f4fb437a3fa199c4f559917a1822
Martin Lehmann <martin@lehmann.tech>
2025-07-03 11:27:16 +0200

    fix: make the team picture a priority so it doesn't jump in after the first page load (which required bumping Astro to 5.10.x)

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,13 +8,14 @@ import lottie from 'astro-integration-lottie';
 export default defineConfig({
   site: 'https://sokkel.io',
 
-  experimental: {
-    responsiveImages: true,
-  },
-
   integrations: [sitemap(), lottie()],
 
+  image: {
+    responsiveStyles: true,
+  },
+
   redirects: {
-    '/2025-06-deck': 'https://www.figma.com/deck/LePYMT44A7Tfq4k0wGwG1K/Sokkel---Pitch-Deck--Presentation-?node-id=14-649&viewport=-130%2C-138%2C0.61&t=ELDthSvGVH51kDVR-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1',
+    '/2025-06-deck':
+      'https://www.figma.com/deck/LePYMT44A7Tfq4k0wGwG1K/Sokkel---Pitch-Deck--Presentation-?node-id=14-649&viewport=-130%2C-138%2C0.61&t=ELDthSvGVH51kDVR-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1',
   },
 });
