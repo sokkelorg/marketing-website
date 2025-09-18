a6b6bd2800cea3da9aae6dd1b0f5e133f1c7435d
Martin Lehmann <martin@lehmann.tech>
2025-09-18 08:17:54 +0200

    refactor: move static redirects to nginx config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import lottie from 'astro-integration-lottie';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sokkel.io',
@@ -13,10 +11,5 @@ export default defineConfig({
 
   image: {
     responsiveStyles: true,
-  },
-
-  redirects: {
-    '/security': '/terms/security',
-    '/privacy': '/terms/privacy',
   },
 });
